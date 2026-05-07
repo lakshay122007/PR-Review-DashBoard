f27d0c4bf80b837d0d6262abbff8d85dfe3a27b9
lakshay122007 <lakshay11goyal@gmail.com>
2026-05-07 22:17:12 +0530

    added required folder structure, added dashboard and login page components

---
+++ pr-dashboard/src/pages/LoginPage.jsx
@@ -0,0 +1,9 @@
+function LoginPage() {
+    return (
+        <div className="min-h-screen bg-gray-950 text-white flex items-center justify-center">
+        <h1 className="text-2xl font-bold">Login Page</h1>
+        </div>
+    )
+}
+
+export default LoginPage
