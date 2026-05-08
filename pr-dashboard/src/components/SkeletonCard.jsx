21ebb2bc3320fcdeaadc863c1a4d702d7707781e
lakshay122007 <lakshay11goyal@gmail.com>
2026-05-08 22:03:53 +0530

    add loading skeleton cards

---
+++ pr-dashboard/src/components/SkeletonCard.jsx
@@ -0,0 +1,10 @@
+function SkeletonCard() {
+    return (
+        <div className="bg-gray-900 rounded-lg p-4 animate-pulse">
+        <div className="h-3 bg-gray-700 rounded w-24 mb-3" />
+        <div className="h-7 bg-gray-700 rounded w-16" />
+        </div>
+    )
+}
+
+export default SkeletonCard
